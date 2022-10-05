7ed24dce7940172bb0dd0d94457434479fc06e0f
Thomas Kraak <thomas.kraak@capgemini.com>
2022-10-05 19:20:18 +0200

    Changed order of seeders data

--- seeders/20220829132840-experiences.js
+++ seeders/20220829132840-experiences.js
@@ -6,12 +6,42 @@ module.exports = {
       "experiences",
       [
         {
-          title: "Digital Engineer - Full-stack developer",
-          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787675/portfolio-pictures/capgemini_ag0iv7.jpg",
-          company: "Capgemini",
-          period: `Feb 2021 - Present`,
+          title: "Supervisoer Building and housing",
+          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/gemeente-amsterdam_rvup3j.png",
+          company: "Gemeente Amsterdam, Stadsdeel Centrum · Full-time",
+          period: "Jul 2011 - Feb 2012 · 8 months",
           description:
-            "Working for different clients as a consultant and full-stack developer. See 'Projects' for more information.",
+            "Checking the condition of buildings/housing, state of the construction/fundament and checking building projects are following the regulations in Amsterdam.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Underwriter Technical Insurances CAR",
+          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/deltalloyd_zhpmlv.png",
+          company: "Delta Lloyd, Technische Verzekeringen",
+          period: "Feb 2012 - May 2015 · 3 yrs 4 months",
+          description: `Assessing, determining conditions and accepting insurances for the construction of buildings and installations. 
+            Implementing a new program to make quotations and keep the database of insurances for the company using Scrum method.`,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Project Manager",
+          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/sineth_dvvvdi.svg",
+          company: "Sineth Engineering B.V.",
+          period: "Jun 2015 - Aug 2018 · 3 yrs 3 months",
+          description:
+            "Supervising building projects are carried out as planned and consulting contractors, including maintaining contact with clients and budget management.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Building Engineer",
+          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787672/portfolio-pictures/AVB_yoy8ko.png",
+          company: "AVB (Amsterdams Vastgoed Beheer)",
+          period: "Sep 2018 - Jul 2020 · 1 yr 11 months",
+          description:
+            "Preparing and applying permits for building/renovation projects, including architectural drawings and discussing plans and drawings with clients.",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -27,42 +57,12 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          title: "Building Engineer",
-          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787672/portfolio-pictures/AVB_yoy8ko.png",
-          company: "AVB (Amsterdams Vastgoed Beheer)",
-          period: "Sep 2018 - Jul 2020 · 1 yr 11 months",
+          title: "Digital Engineer - Full-stack developer",
+          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787675/portfolio-pictures/capgemini_ag0iv7.jpg",
+          company: "Capgemini",
+          period: `Feb 2021 - Present`,
           description:
-            "Preparing and applying permits for building/renovation projects, including architectural drawings and discussing plans and drawings with clients.",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Project Manager",
-          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/sineth_dvvvdi.svg",
-          company: "Sineth Engineering B.V.",
-          period: "Jun 2015 - Aug 2018 · 3 yrs 3 months",
-          description:
-            "Supervising building projects are carried out as planned and consulting contractors, including maintaining contact with clients and budget management.",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Underwriter Technical Insurances CAR",
-          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/deltalloyd_zhpmlv.png",
-          company: "Delta Lloyd, Technische Verzekeringen",
-          period: "Feb 2012 - May 2015 · 3 yrs 4 months",
-          description: `Assessing, determining conditions and accepting insurances for the construction of buildings and installations. 
-            Implementing a new program to make quotations and keep the database of insurances for the company using Scrum method.`,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Inspecteur Bouwen & Wonen",
-          logo: "https://res.cloudinary.com/leaves-client/image/upload/v1661787676/portfolio-pictures/gemeente-amsterdam_rvup3j.png",
-          company: "Gemeente Amsterdam, Stadsdeel Centrum · Full-time",
-          period: "Jul 2011 - Feb 2012 · 8 months",
-          description:
-            "Checking the condition of buildings/housing, state of the construction/fundament and checking building projects are following the regulations in Amsterdam.",
+            "Working for different clients as a consultant and full-stack developer. See 'Projects' for more information.",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
--- seeders/20220829133228-projects.js
+++ seeders/20220829133228-projects.js
@@ -6,24 +6,13 @@ module.exports = {
       "projects",
       [
         {
-          project: "PRO - Dutch Government websites",
+          project: "Easy Coding",
           image:
-            "https://res.cloudinary.com/leaves-client/image/upload/v1661787684/portfolio-pictures/rijksoverheid_jzle2b.png",
-          company: "Ministerie van Algemene Zaken",
-          description:
-            "Improving, maintaining and upgrading the websites of the Dutch government. This included setting up the MVP CI/CD Pipeline and upgrading the Bloomreach CMS system",
-          url: "",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          project: "Rijksportaal - Intranet Dutch Government",
-          image:
-            "https://res.cloudinary.com/leaves-client/image/upload/v1661787677/portfolio-pictures/rijksportaal_rvnusg.jpg",
-          company: "Ministerie van Binnenlandse Zaken",
-          description:
-            "Building the new intranet for the Dutch government using Bloomreach CMS headless.",
-          url: "",
+            "https://res.cloudinary.com/leaves-client/image/upload/v1661787679/portfolio-pictures/easycoding_bkzkys.png",
+          company: "Codaisseur Academy",
+          description: `Group assignment in the final weeks of the academy to create a website from scratch with 3 people in 1 week.
+          This is an app created for developers who need help how to implement a new feature or dependency. Through this app a user can upload a post to help others code in the easiest possible way, or even request a matter that they have trouble with, so eventually another user can and post something about it.`,
+          url: "https://easy-coding.netlify.app/",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -39,13 +28,24 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          project: "Easy Coding",
+          project: "Rijksportaal - Intranet Dutch Government",
           image:
-            "https://res.cloudinary.com/leaves-client/image/upload/v1661787679/portfolio-pictures/easycoding_bkzkys.png",
-          company: "Codaisseur Academy",
-          description: `Group assignment in the final weeks of the academy to create a website from scratch with 3 people in 1 week.
-          This is an app created for developers who need help how to implement a new feature or dependency. Through this app a user can upload a post to help others code in the easiest possible way, or even request a matter that they have trouble with, so eventually another user can and post something about it.`,
-          url: "https://easy-coding.netlify.app/",
+            "https://res.cloudinary.com/leaves-client/image/upload/v1661787677/portfolio-pictures/rijksportaal_rvnusg.jpg",
+          company: "Ministerie van Binnenlandse Zaken",
+          description:
+            "Building the new intranet for the Dutch government using Bloomreach CMS headless.",
+          url: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          project: "PRO - Dutch Government websites",
+          image:
+            "https://res.cloudinary.com/leaves-client/image/upload/v1661787684/portfolio-pictures/rijksoverheid_jzle2b.png",
+          company: "Ministerie van Algemene Zaken",
+          description:
+            "Improving, maintaining and upgrading the websites of the Dutch government. This included setting up the MVP CI/CD Pipeline and upgrading the Bloomreach CMS system",
+          url: "",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
